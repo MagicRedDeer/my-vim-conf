02bc55afd7afcc9c944b04b0586f75916e501ab5
Talha Ahmed <talha.ahmed@gmail.com>
2021-12-17 09:27:41 +0500

    starting to configure which_key

--- lua/nvim_plugin_settings/which_key.lua
+++ lua/nvim_plugin_settings/which_key.lua
@@ -1,20 +1,38 @@
 local module = {}
 
 module.install = function(use)
- use {
-  "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end
+    use {
+    "folke/which-key.nvim",
+    config = function()
+        require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        }
+    end
 }
 end
 
 module.configure = function ()
     vim.o.timeoutlen = 300
+    local mappings = {
+        f = {
+            name = "file"
+        },
+        e = {
+            name = "edit"
+        },
+        s = {
+            name = "select/other",
+            a = { "Select All" }
+        }
+    }
+    local opts = {
+        prefix = "<leader>"
+    }
+
+    require("which-key").register(mappings, opts)
+
 end
 
 return module
